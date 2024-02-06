f087d346fddd87810de721845627296ed5e5a453
Tony Eder <tonyeder11@gmail.com>
2024-02-06 08:18:18 -0800

    3rd test passed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -6,7 +6,12 @@ def batch_badge_creator(names):
 
 
 def assign_rooms(names):
-    return None
+    list = []
+    index = 1
+    for name in names:
+        list.append(f"Hello, {name}! You'll be assigned to room {index}!")
+        index += 1
+    return list
 
 def printer(names):
     return None
